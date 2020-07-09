e42c547d07054e9df2d964c4cea5469ff230dbff
Manuel De La Mora <Manuel@defaults-mbp.attlocal.net>
2020-07-09 16:28:46 -0700

    Completed histograp, markov and crack_caesar for Thursday mvp and finishing sumdiff.

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -2,4 +2,35 @@
 # decode it.
 
 # Your code here
+with open('applications/crack_caesar/ciphertext.txt') as f:
+    words = f.read()
+    split_words = words.split()
 
+capitals = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+norm_freq = ['E', 'T', 'A', 'O', 'H', 'N', 'R', 'I', 'S', 'D', 'L', 'W', 'U', 'G', 'F', 'B', 'M', 'Y', 'C', 'P', 'K', 'V', 'Q', 'J', 'X', 'Z']
+letters = dict()
+decode_table = dict()
+decoded_message = list()
+
+for word in split_words:
+    for letter in word:
+        if letter in capitals:
+            letters[letter] = letters.get(letter, 0) + 1
+
+pairs = list(letters.items())
+pairs.sort(key = lambda e: e[1], reverse = True)
+
+for i in range(len(norm_freq)):
+    decode_table[pairs[i][0]] = norm_freq[i]
+
+for word in split_words:
+    new_word = ""
+    for letter in word:
+        if letter not in capitals:
+            new_word += letter
+        if letter in capitals:
+            new_letter = decode_table[letter]
+            new_word += new_letter
+    decoded_message.append(new_word)
+
+print(' '.join(decoded_message))
--- applications/sumdiff/sumdiff.py
+++ applications/sumdiff/sumdiff.py
@@ -6,10 +6,16 @@ f(a) + f(b) = f(c) - f(d)
 #q = set(range(1, 10))
 #q = set(range(1, 200))
 q = (1, 3, 4, 7, 12)
-
+lookup_table = dict()
 
 def f(x):
     return x * 4 + 6
 
 # Your code here
+for i in range(len(q) - 1):
+    for j in range(i, len(q)):
+        lookup_table[(q[i], "+", q[j])] = f(q[i]) + f(q[j])
+        lookup_table[(q[i], "-", q[j])] = f(q[i]) - f(q[j])
+        lookup_table[(q[j], "-", q[i])] = f(q[j]) - f(q[i])
 
+print(lookup_table)
