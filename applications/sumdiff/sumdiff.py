020272c5bf747905b25d18737e02ae0aee0bd8af
Manuel De La Mora <Manuel@defaults-mbp.attlocal.net>
2020-07-09 16:55:36 -0700

    Completed sumdiff and made MVP for day 4 of the repo.

--- applications/sumdiff/sumdiff.py
+++ applications/sumdiff/sumdiff.py
@@ -6,7 +6,8 @@ f(a) + f(b) = f(c) - f(d)
 #q = set(range(1, 10))
 #q = set(range(1, 200))
 q = (1, 3, 4, 7, 12)
-lookup_table = dict()
+positive_lookup_table = dict()
+negative_lookup_table = dict()
 
 def f(x):
     return x * 4 + 6
@@ -14,8 +15,12 @@ def f(x):
 # Your code here
 for i in range(len(q) - 1):
     for j in range(i, len(q)):
-        lookup_table[(q[i], "+", q[j])] = f(q[i]) + f(q[j])
-        lookup_table[(q[i], "-", q[j])] = f(q[i]) - f(q[j])
-        lookup_table[(q[j], "-", q[i])] = f(q[j]) - f(q[i])
+        positive_lookup_table[(q[i], "+", q[j])] = f(q[i]) + f(q[j])
+        positive_lookup_table[(q[j], "+", q[i])] = f(q[j]) + f(q[i])
+        negative_lookup_table[(q[i], "-", q[j])] = f(q[i]) - f(q[j])
+        negative_lookup_table[(q[j], "-", q[i])] = f(q[j]) - f(q[i])
 
-print(lookup_table)
+for positive_key, positive_value in positive_lookup_table.items():
+    for negative_key, negative_value in negative_lookup_table.items():
+        if positive_value == negative_value:
+            print(f"f({positive_key[0]}) + f({positive_key[2]}) = f({negative_key[0]}) - f({negative_key[2]})")
